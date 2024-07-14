76974baa5dadef9e456f6826476e367903789664
rbasanigit <113279450+rbasanigit@users.noreply.github.com>
2024-07-14 10:30:51 +0530

    NumberGuess.c

--- NumberGuess.c
+++ NumberGuess.c
@@ -1,98 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-void user_guess(int num, int *user_points) {
-    int guess, ch = 5;
-    printf("\nUser's turn to guess:\n");
+int comp_points = 0;
+int user_points = 0;
 
-    while (ch != 0) {
-        printf("Enter your guess: ");
-        scanf("%d", &guess);
-
-        if (num == guess) {
-            printf("Correct! The number is %d\n", num);
-            (*user_points)++;
-            printf("Your score is %d.\n", *user_points);
+void comp_guess() {
+    int begin = 1;
+    int end = 100;
+    printf("Computer's turn to guess the number (1-100):\n");
+    for (int i = 0; i < 5; i++) {
+        int mid = (begin + end) / 2;
+        printf("%d\n", mid);
+        char hint[10];
+        printf("Enter if the guess is greater or lesser, or correct:");
+        scanf("%s", hint);
+        if (strcmp(hint, "greater") == 0) {
+            begin = mid + 1;
+        } 
+        else if (strcmp(hint, "lesser") == 0) {
+            end = mid - 1;
+        } 
+        else if (strcmp(hint, "correct") == 0) {
+            printf("Computer wins!!\n");
+            comp_points++;
             break;
-        } else if (num > guess) {
-            printf("The number is greater than your guess. Try again!\n");
-        } else {
-            printf("The number is lesser than your guess. Try again!\n");
+        } 
+        else {
+            printf("Please enter 'greater', 'lesser', or 'correct':\n");
         }
-        ch--;
     }
-
-    if (ch == 0 && num != guess) {
-        printf("You Lost! The correct number was %d.\n", num);
+    if (comp_points == 0) {
+        printf("Computer couldn't guess the number.\n");
     }
 }
 
-void computer_guess(int num, int *comp_points) {
-    int guess, ch = 5;
-    printf("\nComputer's turn to guess:\n");
-
-    while (ch != 0) {
-        guess = rand() % 100 + 1;
-        printf("Computer guesses: %d\n", guess);
-
-        if (num == guess) {
-            printf("Correct! The number is %d\n", num);
-            (*comp_points)++;
-            printf("Computer's score is %d.\n", *comp_points);
+void user_guess() {
+    srand(time(NULL));
+    int num = rand() % 100 + 1;
+    printf("User's turn to guess a number (1-100):\n");
+    for (int i = 0; i < 5; i++) {
+        int guess;
+        printf("Enter your guess:");
+        scanf("%d", &guess);
+        if (guess < num) {
+            printf("The number is greater. Try again!\n");
+        } 
+        else if (guess > num) {
+            printf("The number is lesser. Try again!\n");
+        } 
+        else if (guess == num) {
+            printf("User wins!!. The number is %d.\n", num);
+            user_points++;
             break;
-        } else if (num > guess) {
-            printf("The number is greater than computer's guess.\n");
-        } else {
-            printf("The number is lesser than computer's guess.\n");
+        } 
+        else {
+            printf("Please enter a valid number\n");
         }
-        ch--;
     }
-
-    if (ch == 0 && num != guess) {
-        printf("Computer Lost! The correct number was %d.\n", num);
+    if (user_points == 0) {
+        printf("Couldn't guess the number. The correct number was %d\n", num);
     }
 }
 
 int main() {
-    int num, play = 1, user_points = 0, comp_points = 0, rounds = 0;
-    char continue_play;
-
-    printf("Press 1 to play: ");
-    scanf("%d", &play);
-
-    srand(time(0)); 
-
-    while (play == 1 && rounds < 5) {
-        num = rand() % 100 + 1;
-
-        user_guess(num, &user_points);
-
-        num = rand() % 100 + 1;
-
-        computer_guess(num, &comp_points);
-
+    int rounds = 0;
+    while (rounds < 5) {
+        user_guess();
+        comp_guess();
         rounds++;
-        printf("\nRound %d complete. User points: %d, Computer points: %d\n", rounds, user_points, comp_points);
-
+        printf("Round %d complete. User points: %d, Computer points: %d\n", rounds, user_points, comp_points);
         if (rounds < 5) {
-            printf("Do you want to continue playing? (y/n): ");
-            scanf(" %c", &continue_play); 
-            if (continue_play == 'n' || continue_play == 'N') {
+            char continue_play[2];
+            printf("Do you want to continue playing? (y/n):");
+            scanf("%s", continue_play);
+            if (continue_play[0] == 'n') {
                 break;
             }
         }
     }
-
+    
     if (user_points > comp_points) {
-        printf("\nUser wins with %d points!\n", user_points);
-        printf("Computer loses with %d points.\n", comp_points);
-    } else if (comp_points > user_points) {
-        printf("\nComputer wins with %d points!\n", comp_points);
-        printf("User loses with %d points.\n", user_points);
-    } else {
+        printf("\nUser wins with %d points!\nComputer loses with %d points.\n", user_points, comp_points);
+    } 
+    else if (comp_points > user_points) {
+        printf("\nComputer wins with %d points!\nUser loses with %d points.\n", comp_points, user_points);
+    } 
+    else {
         printf("\nIt's a tie! Both user and computer have %d points.\n", user_points);
     }
-
+    
     return 0;
 }
+
